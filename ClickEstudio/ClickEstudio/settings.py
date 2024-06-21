878a66d4549611103d9e00278cd2fdd02d96c565
Wandy Olivares <one.wandy@icloud.com>
2024-06-21 13:00:36 -0400

    Actualizar configuración de ALLOWED_HOSTS y agregar soporte para lazy loading de imágenes

--- ClickEstudio/ClickEstudio/settings.py
+++ ClickEstudio/ClickEstudio/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = [
     'clickestudios.com',
      '64.23.182.234',
      'www.clickestudios.com',
-     "*"
+     
      
 ]
 # CSRF_TRUSTED_ORIGINS = 'https://6f6c-2001-1308-2740-ae00-a534-b5c8-c8c4-a8a7.ngrok-free.app'
