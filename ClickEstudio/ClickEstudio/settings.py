a6f38920e2308b34bc9a058e35188a597d4dcd12
Wandy Olivares <129364431+untal-wandy@users.noreply.github.com>
2024-08-15 15:33:10 -0400

    NMA

--- ClickEstudio/ClickEstudio/settings.py
+++ ClickEstudio/ClickEstudio/settings.py
@@ -13,7 +13,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-
+BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -114,9 +115,10 @@ USE_TZ = True
 \
 STATIC_URL = '/static/'
 
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    # otros directorios adicionales
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
